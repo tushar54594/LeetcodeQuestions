a06fa75c619fe061c081870ac43a702144914af5
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-01-10 00:33:12 +0530

    Time: 3 ms (25.09%), Space: 5.9 MB (81.05%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -6,10 +6,12 @@ public:
         
         if(n==0)
             return 1;
+        
         while(m>0){
             mask = (mask<<1) | 1;
             m = m>>1;
         }
-        return (~n) & mask;
+        int ans = (~n) & mask;
+        return ans;
     }
 };
