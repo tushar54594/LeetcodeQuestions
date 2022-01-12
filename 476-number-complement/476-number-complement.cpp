8d9cf033340ae77a26795381b116678d31c173dc
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-01-12 09:48:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (42.43%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int m = num;
+        int mask = 0;
+        
+        if(num==0)
+            return 1;
+        
+        while(m>0){
+            mask = (mask<<1) | 1;
+            m = m>>1;
+        }
+        int ans = (~num) & mask;
+        return ans;
+    }
+};
