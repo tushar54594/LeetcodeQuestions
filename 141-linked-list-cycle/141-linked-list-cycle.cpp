fa6dc0f9c374ea281afbfe9143e8a0d46e616832
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-03-03 18:03:31 +0530

    Time: 12 ms (78.38%), Space: 8.1 MB (17.51%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL)
+        if(head == NULL)
             return false;
         
         ListNode *slow = head;
