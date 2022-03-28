9dc3b7de296801a124e7ff61e7a82dcaccec5a77
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-03-28 08:24:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (26.86%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans = sqrt(x);
-        return ans;
+        long l = 0;
+        long h = x;
+        while(l<=h){
+            long mid = l + (h-l)/2;
+            if(mid*mid <= x)
+                l = mid + 1;
+            else
+                h = mid - 1;
+        }
+        return h;
     }
 };
