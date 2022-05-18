4a897e4b4b16df9ff439f707c9685991622f8814
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-05-19 00:56:57 +0530

    Time: 35 ms (70.75%), Space: 24.8 MB (99.33%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,17 +3,11 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        if(n==k)
+        if(n == k)
             return;
-            
-        vector<int> ans;
         
-        for(int i = n-k; i<n; i++)
-            ans.push_back(nums[i]);
-        
-        for(int i=0; i<n-k; i++)
-            ans.push_back(nums[i]);
-        
-        nums = ans;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
