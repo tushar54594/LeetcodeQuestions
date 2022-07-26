bc91552a79db7cc5bbef977d6816d539b2641843
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-07-26 18:48:29 +0530

    Time: 12 ms (65.55%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.size() != t.size())
+            return false;
         
-        if(s == t)
-            return true;
+        vector<int> arr(26,0); //since only lowercase english letters are present
+        for(int i=0; i<s.size(); i++)
+        {
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
         
-        return false;
+        //if the strings are anagrams. then each index of array will have 0 value so if any index have non zero value, then it is not an anagram
+        for(int i=0; i<26; i++)
+        {
+            if(arr[i])
+                return false;
+        }
+        
+        return true;
     }
 };
