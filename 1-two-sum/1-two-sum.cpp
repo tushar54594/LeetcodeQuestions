e6d050c17271ff1392528d9536dff3584eea221f
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-08-31 00:57:20 +0530

    Time: 17 ms (73.82%), Space: 10.5 MB (57.73%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i =0; i<nums.size(); i++){
-            int n1 = nums[i];
-            for(int j = i + 1; j<nums.size(); j++){
-                int n2 = nums[j];
-                if(target == (n1 + n2)){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
+        vector<int> ans, res;
+        res = nums;
+        
+        sort(res.begin(), res.end());
+        
+        int num1, num2; 
+                    
+        int i = 0, j = res.size()-1;
+        while(i<res.size() && j>=0)
+        {
+            if(res[i] + res[j] == target)
+            {
+                num1 = res[i];
+                num2 = res[j];
+                break;
             }
+            else if(res[i] + res[j] < target)
+                i++;
+            else 
+                j--;
         }
+        
+        //since we sorted the array and now we want the original index of the number which we got as the answer
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(num1 == nums[i]) 
+                ans.push_back(i);
+            else if(num2 == nums[i])
+                ans.push_back(i);
+        }
+        
         return ans;
     }
 };
