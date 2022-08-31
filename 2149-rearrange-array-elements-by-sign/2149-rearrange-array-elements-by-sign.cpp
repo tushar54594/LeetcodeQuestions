55c83204f422628bf1e72a4b1797b3035b44ce29
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-08-31 23:20:34 +0530

    Time: 486 ms (5.03%), Space: 134.6 MB (26.18%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -13,17 +13,19 @@ public:
                 neg.push_back(nums[i]);
         }
         
-        int index = 0;
-        for(int i=0; i<nums.size(); i=i+2)
-        {
-            ans[i] = pos[index++];
-        }
-        
-        index = 0;
-        for(int i=1; i<nums.size(); i=i+2)
-        {
-            ans[i] = neg[index++];
-        }
+        int k = 0;
+	    for(int i=0; i<pos.size(); i++)
+	    {
+	        ans[k] = pos[i];
+	        k = k+2;
+	    }
+	    
+	    k = 1;
+	    for(int i=0; i<neg.size(); i++)
+	    {
+	        ans[k] = neg[i];
+	        k = k+2;
+	    }
         
         return ans;
     }
