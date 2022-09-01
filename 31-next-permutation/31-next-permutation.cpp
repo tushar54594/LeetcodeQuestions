57f5cde2d30190d5c4184775ab841c148c89369e
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-09-01 10:22:25 +0530

    Time: 3 ms (90.35%), Space: 12.2 MB (36.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int index1, index2;
+        
+        //step1
+        for(index1 = n-2; index1>=0; index1--)
+        {
+            if(nums[index1] < nums[index1 + 1])
+                break;
+        }
+        //edge case(If there is no breakpint)
+        if(index1 < 0)
+            reverse(nums.begin(), nums.end());
+        
+        //step2
+        else
+        {
+            for(index2 = n-1; index2>index1; index2--)
+            {
+                if(nums[index2] > nums[index1])
+                    break;
+            }
+            //step3
+            swap(nums[index1], nums[index2]);
+            //step4
+            reverse(nums.begin() + index1 + 1, nums.end());
+        }
     }
 };
