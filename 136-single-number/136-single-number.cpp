bb9a9c1b20836c76b1f3d893dac386a14c3181ea
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-09-02 22:49:12 +0530

    Time: 50 ms (18.84%), Space: 17 MB (53.39%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        //move 2 times ahead in every iteration since adjacent elements should be equal
-        for(int i=1; i<nums.size(); i+=2)
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++)
         {
-            if(nums[i] != nums[i-1])
-                return nums[i-1];
+            ans = ans^nums[i];
         }
-        return nums[nums.size()-1];
+        return ans;
     }
 };
