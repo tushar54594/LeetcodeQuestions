7e3ecad1610a50b9dfdf17dea2c32c31257a6b2b
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-09-04 18:44:14 +0530

    Time: 152 ms (34.86%), Space: 13.4 MB (31.79%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+        
+        //fix a
+        for(int i=0; i<n-3; i++)
+        {
+            //ignore the duplicates
+            if(i == 0 || (i>0 && nums[i] != nums[i-1]))
+            {
+                //fix b
+                for(int j=i+1; j<n-2; j++)
+                {
+                    //ignore the duplicates
+                    if(j == i+1 || (j>i+1 && nums[j] != nums[j-1]))
+                    {
+                        int low = j+1, high = n-1;
+                        long sum = long(target) - nums[i] - nums[j];
+
+                        while(low<high)
+                        {
+                            if(long(nums[low] + nums[high]) == sum)
+                            {
+                                vector<int> arr;
+                                arr.push_back(nums[i]);
+                                arr.push_back(nums[j]);
+                                arr.push_back(nums[low]);
+                                arr.push_back(nums[high]);
+                                ans.push_back(arr);
+
+                                while(low<high && nums[low] == nums[low+1])
+                                    low++;
+                                while(low<high && nums[high] == nums[high-1])
+                                    high--;
+
+                                low++;
+                                high--;
+
+                            }
+                            else if(nums[low] + nums[high] < sum)
+                                low++;
+                            else 
+                                high--;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
