31d02a43b065ef1406aa9fab387211f9a43e9c8d
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-09-30 11:56:39 +0530

    Time: 12 ms (69.16%), Space: 13.7 MB (64.32%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    int firstOcc(vector<int> &nums, int target){
-        int s = 0, e = nums.size() - 1;
-        int ans = -1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                e = mid - 1;
-            }
-            else if(nums[mid] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
-        }
-        return ans;
-    }
-    
-public:
-    int lastOcc(vector<int> &nums, int target){
-        int s = 0, e = nums.size() - 1;
-        int ans = -1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                s = mid + 1;
-            }
-            else if(nums[mid] < target)
-                s = mid + 1;
-            else
-                e = mid - 1;
-        }
-        return ans;
-    }
-    
-public:
     vector<int> searchRange(vector<int>& nums, int target) {
-    vector<int> answer;
-    answer.push_back(firstOcc(nums, target));
-    answer.push_back(lastOcc(nums, target));
-    return answer;
+        vector<int> ans;
+        
+        //first occurence
+        int s = 0;
+        int e = nums.size()-1;
+        int firstOccur = -1; //if we do not found the target, answer should be -1
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+            if(target == nums[mid])
+            {
+                firstOccur = mid;
+                e = mid - 1;
+            }
+            else if(target < nums[mid])
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+        
+        //last occurence
+        s = 0;
+        e = nums.size()-1;
+        int lastOccur = -1;
+        while(s<=e)
+        {
+            int mid = s + (e-s)/2;
+            if(target == nums[mid])
+            {
+                lastOccur = mid;
+                s = mid + 1;
+            }
+            else if(target < nums[mid])
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+        
+        ans.push_back(firstOccur);
+        ans.push_back(lastOccur);
+        return ans;
     }
 };
