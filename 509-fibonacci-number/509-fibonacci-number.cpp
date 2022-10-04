550e11c0af2434f2314675fc4e9fc04a34d27e02
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-04 11:23:41 +0530

    Time: 4 ms (57.40%), Space: 6 MB (38.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,13 +4,16 @@ public:
         if(n <= 1)
             return n;
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        
+        int prev2 = 0;
+        int prev = 1;
+        int curr = -1;
         for(int i=2; i<=n; i++)
-            dp[i] = dp[i-1] + dp[i-2];
+        {
+            curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
         
-        return dp[n];
+        return prev;
     }
 };
