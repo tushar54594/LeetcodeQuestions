b88771d9cab16be57f8e71a137e748313944bec4
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-04 14:37:07 +0530

    Time: 3 ms (36.09%), Space: 6.3 MB (15.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n <= 1)
+    int solve(int n, vector<int> &dp)
+    {
+        if(n == 0)
+            return 0;
+        if(n == 1)
             return 1;
+        if(n == 2)
+            return 2;
         
-        int prev2 = 1;
-        int prev = 1;
-        for(int i=2; i<=n; i++)
-        {
-            int curr = prev2 + prev;
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        if(dp[n] != -1)
+            return dp[n];
+        
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+    
+    int climbStairs(int n) {
+       vector<int> dp(n+1, -1);
+       return solve(n, dp);
     }
 };
