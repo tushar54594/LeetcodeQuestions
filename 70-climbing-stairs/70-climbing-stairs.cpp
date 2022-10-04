03859b5529e6d43804740a42f54005c4ce3c388c
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-04 12:25:13 +0530

    Time: 5 ms (13.76%), Space: 5.7 MB (97.15%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -4,15 +4,14 @@ public:
         if(n <= 1)
             return 1;
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
-        
+        int prev2 = 1;
+        int prev = 1;
         for(int i=2; i<=n; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
         }
-        
-        return dp[n];
+        return prev;
     }
 };
