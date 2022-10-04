73f6863c80369843cca1f49272ae067008d80b56
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-04 11:09:33 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int fibonacci(int n, vector<int> &dp)
-    {
+    int fib(int n) {
         if(n <= 1)
             return n;
         
-        //checking if subproblem has been previously solved
-        if(dp[n] != -1)
-            return dp[n];
-        
-        //storing the ans which is computed for every subproblem
-        return dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
-    }
-    
-    int fib(int n) {
         vector<int> dp(n+1, -1);
-        return fibonacci(n, dp);
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
     }
 };
