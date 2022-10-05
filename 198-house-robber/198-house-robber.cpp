830bd9d8f1b07eab9ec31a441c8ea270e3386029
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-05 11:18:46 +0530

    Time: 10 ms (8.37%), Space: 7.6 MB (95.13%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
+    int solve(int n, vector<int>& nums)
+    {
         int prev2 = 0;
         int prev = nums[0];
         
-        for(int i=1; i<n; i++)
+        for(int i=1; i<=n; i++)
         {
             int pick = nums[i];
             if(i > 1)
@@ -17,5 +17,10 @@ public:
             prev = curr;
         }
         return prev;
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        return solve(n-1, nums);
     }
 };
