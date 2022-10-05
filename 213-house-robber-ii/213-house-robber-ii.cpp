13a1426aa6853d3db8c78a213b0b0387b422af93
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-05 12:07:50 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.55%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -24,14 +24,16 @@ public:
         if(n == 1)
             return nums[0];
         
-        vector<int> temp1(n-1);
-        for(int i=0; i<n-1; i++)
-            temp1.push_back(nums[i]);
+        vector<int> temp1, temp2;
+        for(int i=0; i<n; i++)
+        {
+            if(i != 0) 
+                temp1.push_back(nums[i]);
+            if(i != n-1)
+                temp2.push_back(nums[i]);
+        }
+            
         int ans1 = solve(temp1);
-        
-        vector<int> temp2(n-1);
-        for(int i=1; i<n; i++)
-            temp2.push_back(nums[i]);
         int ans2 = solve(temp2);
         
         return max(ans1, ans2);
