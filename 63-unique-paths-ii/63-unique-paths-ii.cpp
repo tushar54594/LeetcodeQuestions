c1a65b3088b98119638ea97512e72076e6364038
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-07 01:07:43 +0530

    Time: 3 ms (86.83%), Space: 7.9 MB (45.58%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int solve(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp)
     {
-        if(m == 0 && n == 0 && obstacleGrid[m][n] == 0)//we have reached the destination and obstacle is not there
+        if(m>=0 && n>=0 && obstacleGrid[m][n] == 1)
+            return 0;
+        if(m == 0 && n == 0) 
             return 1;
         if(m < 0 || n < 0)
-            return 0;
-        if(obstacleGrid[m][n] == 1)   //obstacle is present
             return 0;
         
         if(dp[m][n] != -1)
