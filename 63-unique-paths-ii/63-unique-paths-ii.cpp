31eb1dafa148d50c038480fa11a83933db201c2c
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-06 23:19:33 +0530

    Time: 9 ms (25.04%), Space: 7.9 MB (13.41%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp)
+    {
+        if(m == 0 && n == 0 && obstacleGrid[m][n] == 0)//we have reached the destination and obstacle is not there
+            return 1;
+        if(m < 0 || n < 0)
+            return 0;
+        if(obstacleGrid[m][n] == 1)   //obstacle is present
+            return 0;
+        
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        int top = solve(m-1, n, obstacleGrid, dp);
+        int left = solve(m, n-1, obstacleGrid, dp);
+        
+        return dp[m][n] = top + left;
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m-1, n-1, obstacleGrid, dp);
+    }
+};
