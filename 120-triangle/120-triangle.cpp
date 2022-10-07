d650a08855c88adf85acfe91b91b768fff7b0d3e
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-07 23:14:47 +0530

    Time: 23 ms (5.81%), Space: 9 MB (32.63%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,23 +2,25 @@ class Solution {
 public: 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<int> front(n, -1);
         
         //base case (Try to think the possible values of j)
         for(int j=0; j<n; j++)
         {
-            dp[n-1][j] = triangle[n-1][j];
+            front[j] = triangle[n-1][j];
         }
         
         for(int i=n-2; i>=0; i--)
         {
+            vector<int> curr(n, -1);
             for(int j=i; j>=0; j--)
             {
-                int v1 = triangle[i][j] + dp[i+1][j];
-                int v2 = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(v1, v2);
+                int v1 = triangle[i][j] + front[j];
+                int v2 = triangle[i][j] + front[j+1];
+                curr[j] = min(v1, v2);
             }
+            front = curr;
         }
-        return dp[0][0];
+        return front[0];
     }
 };
