bb039028ff866ec875a9c493e0e903aaee9fe010
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-15 00:26:57 +0530

    Time: 320 ms (11.26%), Space: 45.1 MB (29.07%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int solve(int index, int amount, vector<int>& coins, vector<vector<int>> &dp)
-    {
-        if(index == 0)
-        {
-            //if the amount is divible by coins[0], then this means that we can achieve are target and the                 number of coins required is amount/coins[0].
-            //Eg. If the target is 12 and coins[0] = 4, then we see that amount is divible by coins[0] so                 answer will be 12 / 4 i.e. 3
-            if((amount%coins[0]) == 0) 
-                return amount/coins[0];
-            else
-                return 1e9;  //not possible so we do not take it
-        }
-        
-        if(dp[index][amount] != -1)
-            return dp[index][amount];
-        
-        int notTake = 0 + solve(index-1, amount, coins, dp);
-        int take = 1e9; //we want min so initialize it to 1e9
-        if(coins[index] <= amount)
-            take = 1 + solve(index, amount-coins[index], coins, dp);
-        //we can take a coin any number of times so we call again index till coins[index] <= target
-        
-        return dp[index][amount] = min(notTake, take);
-    }
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(n-1, amount, coins, dp);
-        if(ans == 1e9)
+         
+        //base case
+        for(int t=0; t<=amount; t++)
+        {
+            if(t%coins[0] == 0)
+                dp[0][t] = t/coins[0];
+            else
+                dp[0][t] = 1e9;
+        }
+        
+        for(int index=1; index<n; index++)
+        {
+            for(int t = 0; t<=amount; t++)
+            {
+                int notTake = 0 + dp[index-1][t];
+                int take = 1e9;
+                if(coins[index] <= t)
+                    take = 1 + dp[index][t-coins[index]];
+                
+                dp[index][t] = min(notTake, take);
+            }
+        }
+        if(dp[n-1][amount] == 1e9)
             return -1;
-            
-        return ans;
+        
+        return dp[n-1][amount];
     }
 };
