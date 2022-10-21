3d71917aaed122e18f3f0d17a9af0da9a96a1ecc
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-21 10:12:53 +0530

    Time: 66 ms (51.49%), Space: 6.5 MB (82.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,22 +4,29 @@ public:
         int n = s.size();
         int m = t.size();
         
-        vector<vector<double>> dp(n+1, vector<double>(m+1, 0));
-        
-        //base case
+        vector<double> prev(m+1, 0), curr(m+1, 0);
+        /*
         for(int i=0; i<=n; i++)
             dp[i][0] = 1;
+        base case for tabulation. We are going to every row and initializing 1st value as 1
+        so similarly we do initalize to 1 for both prev and curr
+        */
+        
+        //base case
+        prev[0] = 1;
+        curr[0] = 1;
         
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=m; j++)
             {
                 if(s[i-1] == t[j-1])
-                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                    curr[j] = prev[j] + prev[j-1];
                 else
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
             }
+            prev = curr;
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
