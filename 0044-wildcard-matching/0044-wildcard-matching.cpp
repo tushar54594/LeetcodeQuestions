8a879b72d27fab306ae7060828c4366cca80f264
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-21 23:21:48 +0530

    Time: 194 ms (16.51%), Space: 11.2 MB (65.81%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
-    bool solve(int i, int j, string &s, string &p, vector<vector<int>> &dp)
-    {
-        //base case
-        //both the strings have to be exhausted for the comparison to be true
-        if(j<0 && i<0)
-            return true;
-        //if the pattern is exhausted but the string is still left, return false
-        if(j<0 && i>=0)
-            return false;
-        //if string is exhausted but pattern is left, then the pattern string should only have '*' then only            it can match with the empty string
-        if(i<0 && j>=0)
-        {
-            for(int a=0; a<=j; a++)
-            {
-                if(p[a] != '*')
-                    return false;
-            }
-            return true;
-        }
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if(s[i] == p[j] || p[j] == '?')
-            return dp[i][j] = solve(i-1, j-1, s, p, dp);
-        if(p[j] == '*')
-        {
-            return dp[i][j] = solve(i, j-1, s, p, dp) || solve(i-1, j, s, p, dp);
-        }
-        
-        return false;
-    }
-    
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n-1, m-1, s, p, dp);
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        
+        //base case
+        dp[0][0] = true;
+        for(int i=1; i<=n; i++)
+            dp[i][0] = false;
+        for(int j=1; j<=m; j++)
+        {
+            int flag = true;
+            for(int a=1; a<=j; a++)
+            {
+                if(p[a-1] != '*')
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1] == p[j-1] || p[j-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+                else
+                    dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
