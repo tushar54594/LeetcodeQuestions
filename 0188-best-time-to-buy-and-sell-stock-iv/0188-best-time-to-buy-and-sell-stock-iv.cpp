19ae166124a38320db9d60963b6e60b7d091ac1f
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 00:57:44 +0530

    Time: 23 ms (49.27%), Space: 14.4 MB (13.67%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -29,7 +29,33 @@ public:
     
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return solve(0, 1, k, prices, dp);
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        
+        
+        for(int index=n-1; index>=0; index--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+                for(int cap=1; cap<=k; cap++) //cap==0 is base case i.e. 0 so ignore it
+                {
+                    int profit = 0;
+                    if(buy) 
+                    {
+                        int val1 = -prices[index] + dp[index+1][0][cap]; //buy(take)
+                        int val2 = 0 + dp[index+1][1][cap]; //notBuy(notTake)
+                        profit = max(val1, val2);
+                    }
+                    else //sell so +ve profit
+                    {
+                        int v1 = prices[index] + dp[index+1][1][cap-1]; //sell(transaction complete)
+                        int v2 = 0 + dp[index+1][0][cap]; //not sell
+                        profit = max(v1, v2);
+                    }
+
+                    dp[index][buy][cap] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
