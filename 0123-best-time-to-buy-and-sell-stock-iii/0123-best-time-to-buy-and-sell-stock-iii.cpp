5fd21b13f47a50a243f860f01934de914cf9ede1
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 00:29:38 +0530

    Time: 898 ms (35.77%), Space: 132.6 MB (47.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
+    int solve(int index, int trans, vector<int>& prices, vector<vector<int>> &dp)
+    {
+        int n = prices.size();
+        //base case
+        if(index == n || trans == 4)   //at most two transactions so 4(i.e. B S B S)
+            return 0; 
+        
+        if(dp[index][trans] != -1)
+            return dp[index][trans];
+        
+        int profit = 0;
+        //trans will be even when we will perform buy and odd when we will perform sell
+        if(trans%2 == 0)  //buy 
+        {
+            int val1 = -prices[index] + solve(index+1, trans+1, prices, dp); //buy(take)
+            int val2 = 0 + solve(index+1, trans, prices, dp); //notBuy(notTake)
+            profit = max(val1, val2);
+        }
+        else //sell so +ve profit
+        {
+            int v1 = prices[index] + solve(index+1, trans+1, prices, dp); //sell
+            int v2 = 0 + solve(index+1, trans, prices, dp); //not sell
+            profit = max(v1, v2);
+        }
+            
+        return dp[index][trans] = profit;
+    }
+    
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
-   
-        //no need of base case as we are already initializing it to 0
-        
-        for(int index=n-1; index>=0; index--)
-        {
-            for(int buy=0; buy<=1; buy++)
-            {
-                for(int cap=1; cap<=2; cap++) //since cap==0 will be 0(base case)
-                {
-                    int profit = 0;
-                    if(buy) 
-                    {
-                        int val1 = -prices[index] + dp[index+1][0][cap]; //buy(take)
-                        int val2 = 0 + dp[index+1][1][cap]; //notBuy(notTake)
-                        profit = max(val1, val2);
-                    }
-                    else //sell so +ve profit
-                    {
-                        int v1 = prices[index] + dp[index+1][1][cap-1]; //sell(transaction complete)
-                        int v2 = 0 + dp[index+1][0][cap]; //not sell
-                        profit = max(v1, v2);
-                    }
-
-                    dp[index][buy][cap] = profit;
-                }
-            }
-        }
-        
-        return dp[0][1][2];
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+        return solve(0, 0, prices, dp);
     }
 };
