800d9ba1ff00d5e0f11a20b4dbb652912a8a7aaf
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 03:07:13 +0530

    Time: 16 ms (19.08%), Space: 11.8 MB (48.97%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,35 +1,33 @@
 class Solution {
-public:
-    int solve(int index, int buy, vector<int>& prices, vector<vector<int>> &dp)
-    {
-        int n = prices.size();
-        //base case
-        if(index >= n)
-            return 0;
-        
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-        
-        int profit = 0;
-        if(buy) 
-        {
-            int val1 = -prices[index] + solve(index+1, 0, prices, dp); //buy(take)
-            int val2 = 0 + solve(index+1, 1, prices, dp); //notBuy(notTake)
-            profit = max(val1, val2);
-        }
-        else //sell so +ve profit
-        {
-            int v1 = prices[index] + solve(index+2, 1, prices, dp); //sell
-            int v2 = 0 + solve(index+1, 0, prices, dp); //not sell
-            profit = max(v1, v2);
-        }
-            
-        return dp[index][buy] = profit;
-    }
-    
+public:  
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(0, 1, prices, dp);
+        vector<vector<int>> dp(n+2, vector<int>(2, 0)); //since we have index+2
+        
+        //base case
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int index=n-1; index>=0; index--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+                int profit = 0;
+                if(buy) 
+                {
+                    int val1 = -prices[index] + dp[index+1][0]; //buy(take)
+                    int val2 = 0 + dp[index+1][1]; //notBuy(notTake)
+                    profit = max(val1, val2);
+                }
+                else //sell so +ve profit
+                {
+                    int v1 = prices[index] + dp[index+2][1]; //sell
+                    int v2 = 0 + dp[index+1][0]; //not sell
+                    profit = max(v1, v2);
+                }
+
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
