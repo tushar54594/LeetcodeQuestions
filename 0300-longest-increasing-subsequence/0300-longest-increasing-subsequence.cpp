e666b7435bf04b436c165df2873e48b8724a1468
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 23:03:19 +0530

    Time: 1592 ms (12.43%), Space: 292.1 MB (15.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int solve(int index, int prev_index, vector<int>& nums, vector<vector<int>> &dp)
-    {
-        int n = nums.size();
-        //base case
-        if(index == n)
-            return 0;
-        
-        if(dp[index][prev_index+1] != -1)
-            return dp[index][prev_index+1];
-        
-        int notTake = 0 + solve(index+1, prev_index, nums, dp);
-        int take = 0;
-        if(prev_index == -1 || nums[index] > nums[prev_index])
-            take = 1 + solve(index+1, index, nums, dp);
-        
-        return dp[index][prev_index+1] = max(notTake, take);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solve(0, -1, nums, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        //we do n+1 since for index=n-1, we will have dp[n][] in notTake case
+        
+        for(int index=n-1; index>=0; index--)
+        {
+            for(int prev_index=index-1; prev_index>=-1; prev_index--)
+            {
+                int notTake = 0 + dp[index+1][prev_index+1];  //do second parameter +1 since coordinate shift
+                int take = 0;
+                if(prev_index == -1 || nums[index] > nums[prev_index])
+                    take = 1 + dp[index+1][index+1];
+
+                dp[index][prev_index+1] = max(notTake, take);
+            }
+        }
+        return dp[0][-1+1];
     }
 };
