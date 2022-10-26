218f6328e2f91e0fee2d51f190c6e95c15e94043
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 23:05:45 +0530

    Time: 876 ms (21.28%), Space: 10.5 MB (50.09%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        //we do n+1 since for index=n-1, we will have dp[n][] in notTake case
+        vector<int> after(n+1, 0), curr(n+1, 0);
         
         for(int index=n-1; index>=0; index--)
         {
             for(int prev_index=index-1; prev_index>=-1; prev_index--)
             {
-                int notTake = 0 + dp[index+1][prev_index+1];  //do second parameter +1 since coordinate shift
+                int notTake = 0 + after[prev_index+1];  //do second parameter +1 since coordinate shift
                 int take = 0;
                 if(prev_index == -1 || nums[index] > nums[prev_index])
-                    take = 1 + dp[index+1][index+1];
+                    take = 1 + after[index+1];
 
-                dp[index][prev_index+1] = max(notTake, take);
+                curr[prev_index+1] = max(notTake, take);
             }
+            after = curr;
         }
-        return dp[0][-1+1];
+        return after[-1+1];
     }
 };
