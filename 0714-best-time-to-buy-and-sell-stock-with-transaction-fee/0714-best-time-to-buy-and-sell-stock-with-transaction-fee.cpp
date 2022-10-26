0939f237bf7b5f3d06f254d7ad6fa84042e5e448
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 12:07:29 +0530

    Time: 372 ms (35.68%), Space: 55.1 MB (76.07%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,32 +2,22 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<int> ahead(2, 0), curr(2, 0);
-        
-        //base case
-        ahead[0] = ahead[1] = 0;
+        int aheadBuy, aheadNotBuy, currBuy, currNotBuy;
+        aheadBuy = aheadNotBuy = 0;
         
         for(int index=n-1; index>=0; index--)
         {
-            for(int buy=0; buy<=1; buy++)
-            {
-                int profit = 0;
-                if(buy) 
-                {
-                    int val1 = -prices[index] + ahead[0]; //buy(take)
-                    int val2 = 0 + ahead[1]; //notBuy(notTake)
-                    profit = max(val1, val2);
-                }
-                else //sell so +ve profit
-                {
-                    int v1 = prices[index] + ahead[1] - fee; //sell
-                    int v2 = 0 + ahead[0]; //not sell
-                    profit = max(v1, v2);
-                }
-                curr[buy] = profit;
-            }
-            ahead = curr;
+            int val1 = -prices[index] + aheadNotBuy; //buy(take)
+            int val2 = 0 + aheadBuy; //notBuy(notTake)
+            currBuy = max(val1, val2);
+               
+            int v1 = prices[index] + aheadBuy - fee; //sell
+            int v2 = 0 + aheadNotBuy; //not sell
+            currNotBuy = max(v1, v2);
+            
+            aheadNotBuy = currNotBuy;
+            aheadBuy = currBuy;
         }
-        return ahead[1];
-    }
+        return aheadBuy;
+    }    
 };
