5cf8144a55e8403c96ca226f52c4325c46b6db6a
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-26 11:55:57 +0530

    Time: 558 ms (14.76%), Space: 91 MB (35.38%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int solve(int index, int buy, vector<int>& prices, int fee, vector<vector<int>> &dp)
-    {
-        int n = prices.size();
-        //base case
-        if(index == n)
-            return 0;
-        
-        if(dp[index][buy] != -1)
-            return dp[index][buy];
-        
-        int profit = 0;
-        if(buy) 
-        {
-            int val1 = -prices[index] + solve(index+1, 0, prices, fee, dp); //buy(take)
-            int val2 = 0 + solve(index+1, 1, prices, fee, dp); //notBuy(notTake)
-            profit = max(val1, val2);
-        }
-        else //sell so +ve profit
-        {
-            int v1 = prices[index] + solve(index+1, 1, prices, fee, dp) - fee; //sell
-            int v2 = 0 + solve(index+1, 0, prices, fee, dp); //not sell
-            profit = max(v1, v2);
-        }
-            
-        return dp[index][buy] = profit;
-    }
-    
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        return solve(0, 1, prices, fee, dp);
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+        
+        //base case
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int index=n-1; index>=0; index--)
+        {
+            for(int buy=0; buy<=1; buy++)
+            {
+                int profit = 0;
+                if(buy) 
+                {
+                    int val1 = -prices[index] + dp[index+1][0]; //buy(take)
+                    int val2 = 0 + dp[index+1][1]; //notBuy(notTake)
+                    profit = max(val1, val2);
+                }
+                else //sell so +ve profit
+                {
+                    int v1 = prices[index] + dp[index+1][1] - fee; //sell
+                    int v2 = 0 + dp[index+1][0]; //not sell
+                    profit = max(v1, v2);
+                }
+
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
