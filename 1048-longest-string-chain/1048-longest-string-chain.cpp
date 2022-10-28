852a9f8f03cc2fc8be34a812570a2a9f4f9add01
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-28 17:48:00 +0530

    Time: 228 ms (41.25%), Space: 13.3 MB (74.42%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool check(string &s1, string &s2)
+    {
+        if(s1.size() != s2.size()+1)  //s1 is longer string and s2 is shorter one
+            return false;
+        
+        //use two pointer approach to check if s1 can be created by adding 1 character to s2
+        int first = 0;
+        int second = 0;
+        while(first < s1.size())  //s1 is bigger
+        {
+            if(second<s2.size() && s1[first] == s2[second])
+            {
+                first++;
+                second++;
+            }
+            else
+                first++;
+        }
+        
+        if(first == s1.size() && second == s2.size())
+            return true;
+        
+        return false;
+    }
+    
+    static bool comp(string &s1, string &s2)
+    {
+        return s1.size() < s2.size();
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        //sort according to length
+        sort(words.begin(), words.end(), comp);  
+        
+        int n = words.size();
+        vector<int> dp(n, 1); //initialize the dp array to 1
+        int maxi = 1;
+        
+        for(int index=0; index<n; index++)
+        {
+            for(int prev=0; prev<index; prev++)
+            {
+                if(check(words[index], words[prev]))
+                    dp[index] = max(1 + dp[prev], dp[index]);
+            }
+            maxi = max(maxi, dp[index]);
+        }
+        return maxi; 
+    }
+};
