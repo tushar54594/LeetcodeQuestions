a45eb24c09721e4b716edd43034b51c63013894d
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-30 22:43:28 +0530

    Time: 713 ms (55.26%), Space: 10.3 MB (74.97%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -24,7 +24,22 @@ public:
         nums.push_back(1); //insert 1 at end
         nums.insert(nums.begin(), 1); //insert 1 at beginning
         
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return f(1, n, nums, dp);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        
+        for(int i=n; i>=1; i--)
+        {
+            for(int j=i; j<=n; j++)
+            {
+                int maxi = INT_MIN;
+                for(int index=i; index<=j; index++)
+                {
+                    int coins = nums[i-1]*nums[index]*nums[j+1] + dp[i][index-1] + dp[index+1][j];
+                    maxi = max(maxi, coins);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
     } 
 };
