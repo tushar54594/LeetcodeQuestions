d5aa364142078f660b316abb55e4e0291fdf0e59
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-31 01:07:44 +0530

    Time: 424 ms (53.36%), Space: 6.6 MB (75.73%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -14,31 +14,24 @@ public:
         }
         return true;
     }
-    
-    int f(int i, int n, string &s, vector<int> &dp)
-    {
-        //base case
-        if(i == n)
-            return 0;
-        
-        if(dp[i] != -1)
-            return dp[i];
-        
-        int mini = INT_MAX;
-        for(int j=i; j<n; j++)
-        {
-            if(isPalindrome(i, j, s))
-            {
-                int cost = 1 + f(j+1, n, s, dp);
-                mini = min(mini, cost);
-            }
-        }
-        return dp[i] = mini;
-    }
-    
+ 
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp) - 1;
+        vector<int> dp(n+1, 0);
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            int mini = INT_MAX;
+            for(int j=i; j<n; j++)
+            {
+                if(isPalindrome(i, j, s))
+                {
+                    int cost = 1 + dp[j+1];
+                    mini = min(mini, cost);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0] - 1;
     }
 };
