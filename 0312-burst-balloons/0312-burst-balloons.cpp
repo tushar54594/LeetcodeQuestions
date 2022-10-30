d2024d77ba680229bd20f6a9fc9883642a1a3715
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-10-30 22:43:38 +0530

    Time: 664 ms (64.56%), Space: 10.3 MB (74.97%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,24 +1,5 @@
 class Solution {
-public:
-    int f(int i, int j, vector<int>& nums, vector<vector<int>> &dp)
-    {
-        //base case
-        if(i > j)
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int maxi = INT_MIN;
-        for(int index=i; index<=j; index++)
-        {
-            int coins = nums[i-1]*nums[index]*nums[j+1] + f(i, index-1, nums, dp) + f(index+1, j, nums, dp);
-            maxi = max(maxi, coins);
-        }
-        
-        return dp[i][j] = maxi;
-    }
-    
+public: 
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.push_back(1); //insert 1 at end
