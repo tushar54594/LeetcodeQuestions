90443bd524bde28dd7028c7f92c3f65d12e33bc6
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-11-03 23:51:57 +0530

    Time: 699 ms (19.98%), Space: 6.6 MB (75.39%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,37 +1,56 @@
 class Solution {
-public:
-    bool isPalindrome(int start, int end, string &s)
-    {
-        while(start<end)
-        {
-            if(s[start] != s[end])
-                return false;
-            else
-            {
-                start++;
-                end--;
-            }
-        }
-        return true;
-    }
- 
-    int minCut(string s) {
-        int n = s.size();
-        vector<int> dp(n+1, 0);
+    public:
+    
+    bool isPalindrome(int i, int j ,string &s){
         
-        for(int i=n-1; i>=0; i--)
-        {
-            int mini = INT_MAX;
-            for(int j=i; j<n; j++)
-            {
-                if(isPalindrome(i, j, s))
-                {
-                    int cost = 1 + dp[j+1];
-                    mini = min(mini, cost);
-                }
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
             }
-            dp[i] = mini;
+            i++;
+            j--;
         }
-        return dp[0] - 1;
+        
+        return true;
+        
+    }
+    
+    int fun(int i , int n ,string&s,vector<int>&dp){
+        
+        if(i==n){
+            return 0;
+        }
+        
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        
+    int mini = INT_MAX;
+        
+        for(int j=i; j<n; j++){
+            
+            if(isPalindrome(i,j,s)){
+                int cost  = 1 + fun(j+1,n,s,dp); 
+                mini = min(mini,cost); 
+            }
+            
+            
+            
+        }
+        
+         return dp[i] =  mini;
+        
+        
+    }
+    
+public:
+    int minCut(string s) {
+        
+        int n= s.length();
+        
+        vector<int>dp(n,-1);
+        
+        return fun(0,n,s,dp)-1;
+        
     }
 };
