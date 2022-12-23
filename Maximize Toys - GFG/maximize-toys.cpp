fb6e29028a12f14d0460ccf7d4c51ff375da9ebd
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2022-12-23 23:40:03 +0530

    Added solution - LeetHub

---
+++ Maximize Toys - GFG/maximize-toys.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int toyCount(int N, int K, vector<int> arr)
+    {
+        int ans = 0;
+        sort(arr.begin(), arr.end());
+        for(int i=0; i<N; i++)
+        {
+            if(K >= arr[i])
+            {
+                ans++;
+                K -= arr[i];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, K;
+        cin>>N>>K;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.toyCount(N, K, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
