a519af417cb4bfc62db49e9eb16767e37fe98476
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2023-01-10 18:18:55 +0530

    Time: 3 ms (45.87%), Space: 5.9 MB (46.01%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
-    bool solve(int n, vector<int> &dp)
-    {
-        if(n == 1)
-            return false;
-        if(dp[n] != -1)
-            return dp[n];
-        else
-        {
-            for(int i=1; i*i<=n; i++)
-            {
-                if(n%i == 0)
-                {
-                    if(solve(n-i, dp) == false) 
-                        return dp[n] = true;
-                }
-            }
-            return dp[n] = false;
-        }
-    }
-    
     bool divisorGame(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        return n%2 == 0; 
+        //even no, then alice will win the game 
+        //odd no, then alice will lose the game
     }
 };
