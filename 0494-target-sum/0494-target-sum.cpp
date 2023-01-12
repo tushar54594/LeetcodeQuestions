a1975d04e259641ecfe8e25a63208fca7fd3483d
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2023-01-12 11:43:17 +0530

    Time: 771 ms (26.73%), Space: 9.1 MB (81.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
-    int solve(int index, int target, vector<int> &num, vector<vector<int>> &dp)
+    int solve(int index, vector<int>& nums, int target)
     {
         if(index == 0)
         {
-            if(target == 0 && num[0] == 0)
-                return 2;  //we will have 2 ways either take it or not take it
-            if(target == 0 || target == num[0])
+            if(target == nums[0] || target == -nums[0])
                 return 1;
-
             return 0;
         }
-
-        if(dp[index][target] != -1)
-            return dp[index][target];
-
-        int notTake = solve(index-1, target, num, dp);
-        int take = 0;
-        if(num[index] <= target)
-            take = solve(index-1, target-num[index], num, dp);
-
-        return dp[index][target] = (notTake + take);
-    }
- 
-    int findWays(vector<int> &num, int tar)
-    {
-        int n = num.size();
-        vector<vector<int>> dp(n, vector<int>(tar+1, -1));
-        return solve(n-1, tar, num, dp);
-    }
-    
-    int countPartitions(int n, int d, vector<int> &arr) {
-        int totalSum = 0;
-        for(int i=0; i<n; i++)
-            totalSum += arr[i];
-
-        //edge case
-        if((totalSum - d < 0) || ((totalSum-d) %2 != 0))
-            return 0;
-
-        return findWays(arr, (totalSum-d)/2);
+        
+        int pos = solve(index-1, nums, target - nums[index]);
+        int neg = solve(index-1, nums, target + nums[index]);
+        return pos + neg;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        return countPartitions(nums.size(), target, nums);
+        int n = nums.size();
+        int ans = solve(n-1, nums, target);
+        if(nums[0] == 0)
+            return 2*ans;
+        
+        return ans;
     }
 };
