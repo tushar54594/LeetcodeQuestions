104001caf8fc2962e3e6c7ccf6c51ce0158d0f02
Tushar Pathak <94536522+tushar54594@users.noreply.github.com>
2023-01-21 00:18:49 +0530

    Time: 4 ms (54.47%), Space: 6.8 MB (19.51%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -3,26 +3,21 @@ public:
     int countGoodSubstrings(string s) {
         int i = 0;
         int j = 0;
-        
         int ans = 0;
-        unordered_map<char, int> m;
+        map<char, int> mp;
         while(j < s.size())
         {
-            if(m.find(s[j]) == m.end())
-                m[s[j]] = 1;
-            else
-                m[s[j]]++;
-            
+            mp[s[j]]++;
             if(j-i+1 < 3)
                 j++;
             else if(j-i+1 == 3)
             {
-                if(m.size() == 3)
+                if(mp.size() == 3)
                     ans++;
                 
-                m[s[i]]--;
-                if(m[s[i]] == 0)
-                    m.erase(s[i]);
+                mp[s[i]]--;
+                if(mp[s[i]] == 0)
+                    mp.erase(s[i]);
                 
                 i++;
                 j++;
