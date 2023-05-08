cdd07fa048d463d42cd76951dd1bf869d62ec654
Tushar Pathak <tusharpathak2503@gmail.com>
2023-05-09 01:03:55 +0530

    Time: 95 ms (62.85%), Space: 14 MB (77.34%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        
+        unordered_set<string> s(wordList.begin(), wordList.end());
+        s.erase(beginWord);
+        
+        while(!q.empty())
+        {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+            
+            if(word == endWord) 
+                return steps;
+            
+            for(int i=0; i<word.size(); i++)
+            {
+                char original = word[i];
+                for(char ch='a'; ch<='z'; ch++)
+                {
+                    word[i] = ch;
+                    if(s.find(word) != s.end())
+                    {
+                        s.erase(word);
+                        q.push({word, steps+1});
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        
+        return 0;
+    }
+};
+
+/*
+Level order(BFS) -> path will be minimum
+*/
