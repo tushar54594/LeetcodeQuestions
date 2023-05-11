15aaf7e0900d48bd9ae768c3a416a85e9eba48c0
Tushar Pathak <tusharpathak2503@gmail.com>
2023-05-11 18:48:40 +0530

    Time: 66 ms (85.27%), Space: 43.9 MB (9.22%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        if (functions.length === 0) return x;
+        let ans = x;
+
+        for (let i = functions.length - 1; i >= 0; i--) 
+        {
+          const fun = functions[i];
+
+          ans = fun(ans);
+        }
+
+        return ans;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
