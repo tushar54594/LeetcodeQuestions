7f43ca1cd84c8acf9ef9c5b6117a7ce49f3086f4
Tushar Pathak <tusharpathak2503@gmail.com>
2023-05-13 22:49:23 +0530

    Time: 26 ms (59.87%), Space: 13.7 MB (91.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,56 @@
-class Solution {
+class DisjointSet {
+    vector<int> parent, size;
 public:
-    void dfs(int node, vector<int> adjLs[], vector<int> &vis)
-    {
-        vis[node] = 1;
-        for(auto a : adjLs[node])
-        {
-            if(!vis[a])
-                dfs(a, adjLs, vis);
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
     
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         
-        //create the adjacency list
-        vector<int> adjLs[n];
+        DisjointSet ds(n);
+        
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<n; j++)
             {
-                if(isConnected[i][j] == 1 && i != j)
-                {
-                    adjLs[i].push_back(j);
-                }
+                if(isConnected[i][j] == 1) ds.unionBySize(i, j);
             }
         }
         
-        vector<int> vis(n, 0);
         int ans = 0;
         for(int i=0; i<n; i++)
         {
-            if(!vis[i])
-            {
-                dfs(i, adjLs, vis);
-                ans++;
-            }
+            if(ds.findUPar(i) == i) ans++;
         }
         
         return ans;
