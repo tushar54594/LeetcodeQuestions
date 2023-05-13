a79ea7235d7ee7ca2101ea52455392c5b850b3af
Tushar Pathak <tusharpathak2503@gmail.com>
2023-05-13 19:24:59 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,48 +3,82 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet {
+    vector<int> parent, size;
+public:
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> vis(V, 0);
-        
-        pq.push({0, 0}); //{weight, node}
-        
-        int sum = 0;
-        while(!pq.empty())
+        vector<pair<int, pair<int,int>>> edges;
+        for(int i=0; i<V; i++)
         {
-            auto it = pq.top();
-            pq.pop();
-            
-            int weight = it.first;
-            int node = it.second;
-            
-            if(vis[node] == 1) continue;
-            
-            //add to MST
-            vis[node] = 1;
-            sum += weight;
-            
-            for(auto a : adj[node])
+            for(auto it : adj[i])
             {
-                int adjNode = a[0];
-                int edgeWeight = a[1];
-                if(!vis[adjNode])
-                    pq.push({edgeWeight, adjNode});
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt, {node, adjNode}});
+                /*
+                No need of inserting reverse again as it will be ignored by our data structure
+                */
             }
         }
         
-        return sum;
+        DisjointSet ds(V);
+        
+        sort(edges.begin(), edges.end());
+        int mstWt = 0;
+        for(auto it : edges)
+        {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            //check if they belong to the same component then no need to take it in MST
+            if(ds.findUPar(u) != ds.findUPar(v)) 
+            {
+                mstWt += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+        
+        return mstWt;
     }
-    
-    /*
-    If we want the edges that from the MST also,
-    then store the parent also in the pq
-    */
 };
 
 //{ Driver Code Starts.
