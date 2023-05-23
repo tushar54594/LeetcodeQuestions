b081debd5bb010b057f75db60284f7c5802d6529
Tushar Pathak <tusharpathak2503@gmail.com>
2023-05-23 09:48:05 +0530

    Time: 43 ms (89.30%), Space: 20.4 MB (34.91%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,69 @@
+class DisjointSet {
+    vector<int> parent, size;
+public:
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        
+        int maxRow = 0;
+        int maxCol = 0;
+        //to find dimensions of grid
+        for(auto a : stones)
+        {
+            maxRow = max(maxRow, a[0]);
+            maxCol = max(maxCol, a[1]);
+        }
+        
+        //treat every row amd col as a node
+        DisjointSet ds(maxRow + maxCol + 1);
+        
+        unordered_map<int,int> stoneNodes;
+        for(auto it : stones)
+        {
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxRow + 1;
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+        
+        int cnt = 0;
+        for(auto it : stoneNodes)
+        {
+            if(ds.findUPar(it.first) == it.first) cnt++;
+        }
+        
+        return n - cnt;
+    }
+};
