3455b99429723d734d7672f82132a4b7d02ac223
Tushar Pathak <tusharpathak2503@gmail.com>
2023-07-01 21:37:54 +0530

    Time: 6 ms (90.00%), Space: 6.8 MB (62.54%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+    void solve(int index, vector<int>& cookies, vector<int>& children, int k, int &ans)
+    {
+        if(index == cookies.size()){
+            int unfairness = *max_element(children.begin(), children.end());
+            ans = min(ans, unfairness);
+            return;
+        }
+        
+        for(int i=0; i<k; i++){
+            children[i] += cookies[index];
+            solve(index+1, cookies, children, k, ans);
+            children[i] -= cookies[index];
+            if(children[i] == 0) break;
+        }
+    }
+    
+    
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> children(k, 0);
+        
+        int ans = INT_MAX;
+        solve(0, cookies, children, k, ans);
+        
+        return ans;
+    }
+};
