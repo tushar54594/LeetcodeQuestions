bef12e77c831eef7eb4193094d98689e98f55287
Tushar Pathak <tusharpathak2503@gmail.com>
2023-07-14 17:38:59 +0530

    Time: 69 ms (76.96%), Space: 48.5 MB (40.43%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        
+        int lastPos = n-1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] + i >= lastPos){
+                lastPos = i;
+            }
+        }
+        
+        return lastPos == 0;
+    }
+};
